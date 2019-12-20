e38f30ec4f5b7dbaa7b3060be90a6d6cd9c58410
vangelislykasdim <sarkovar@hotmail.gr>
2019-12-20 12:44:32 +0200

    change1

---
+++ WebApplication1/WebApplication1/Models/MichalisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class MichalisController : Controller
+    {
+        // GET: Michalis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
